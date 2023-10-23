92150bb0a4e02cb4a561b7cba10c8dd0fae0995e
RafaelCSL01 <r.cardoso.10@hotmail.com>
2023-10-23 16:36:14 -0300

    feat: add auth handler to header components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
+import { useSession } from 'next-auth/react';
 import Image from 'next/image'
 
 export default function Home() {
+  const {data} = useSession();
   return (
-    <div>
-
-    </div>
+    <div>{data?.user?.name}</div>
   )
 }
